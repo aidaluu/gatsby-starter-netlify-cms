a7c198d6752a22523afa54c4b59dd7eaa9d30e35
Aida Luuppala <aida.luuppala@gmail.com>
2021-11-21 20:34:42 +0200

    Page configs changed

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -2,12 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { AboutPageTemplate } from '../../templates/about-page'
 
-const AboutPagePreview = ({ entry, widgetFor }) => (
+const AboutPagePreview = ({ entry, getAsset }) => {
+  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
+
+  return (
   <AboutPageTemplate
+    image={getAsset(entry.getIn(['data', 'image']))}
     title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
+    heading={entry.getIn(['data', 'heading'])}
+    description={entry.getIn(['data', 'description'])}
+    intro={{ blurbs }}
+    imagefooter={getAsset(entry.getIn(['data', 'image']))}
   />
-)
+  )
+}
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
